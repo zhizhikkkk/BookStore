97e7440a756bade0445f21ab73b8ff4457e2bd3b
zhizhikkkk <zhanelmamyt@gmail.com>
2024-06-22 07:03:20 +0600

    Create domain project, class Book

---
+++ BookStore.Core/Models/Book.cs
@@ -0,0 +1,60 @@
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public const int MAX_TITLE_LENGTH = 250;
+        public const int MAX_DESCRIPTION_LENGTH = 1000;
+        public const int MIN_PAGES = 1;
+        public const int MAX_PAGES = 10000;
+        public const decimal MIN_PRICE = 0.0m;
+
+        private Book(Guid id, string title, string description, int pages, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Pages = pages;
+            Price = price;
+        }
+
+        public Guid Id { get; }
+        public string Title { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public int Pages { get; } = 0;
+        public decimal Price { get; }
+        
+        //реализация паттерна Фабрика
+        public static (Book book, string error) Create(Guid id, string title, string description, int pages, decimal price)
+        {
+            var error = string.Empty;
+
+            if (string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+            {
+                error = "Title cannot be empty or longer than 250 characters.";
+                return (null, error);
+            }
+
+            if (string.IsNullOrEmpty(description) || description.Length > MAX_DESCRIPTION_LENGTH)
+            {
+                error = "Description cannot be empty or longer than 1000 characters.";
+                return (null, error);
+            }
+
+            if (pages < MIN_PAGES || pages > MAX_PAGES)
+            {
+                error = "Pages must be between 1 and 10000.";
+                return (null, error);
+            }
+
+            if (price < MIN_PRICE)
+            {
+                error = "Price must be greater than or equal to 0.";
+                return (null, error);
+            }
+
+            var book = new Book(id, title, description, pages, price);
+
+            return (book, error);
+        }
+    }
+}
